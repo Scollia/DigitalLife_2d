7723c40049b84c2066d53d41c8d8336fa822f037
Scollia <scollia@mail.ru>
2022-08-31 18:13:32 +0300

    Первый скрипт

---
+++ Assets/Модели/Сценарии/Protista_Script.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Protista_Script : MonoBehaviour
+{
+    public float defence;
+    public float organics;
+    public float minerals;
+    public float energy;
+    public float age;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
