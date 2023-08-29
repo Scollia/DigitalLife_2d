cfc20a5a552ffb34eb75fc2836f3993b74eb813f
Scollia <scollia@mail.ru>
2023-08-29 18:07:54 +0300

    Начало создания игрового объема (аквариума)

--- main/Cell/Cell.go
+++ main/Cell/Cell.go
@@ -17,7 +17,7 @@ embedded_components {
   id: "sprite_Cell_(biology)"
   type: "sprite"
   data: "tile_set: \"/main/Cell/Cell.atlas\"\n"
-  "default_animation: \"Cell_(biology)\"\n"
+  "default_animation: \"Cell_Body\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_MULT\n"
   ""
@@ -37,7 +37,7 @@ embedded_components {
   id: "sprite_Nucleolis_(biology)"
   type: "sprite"
   data: "tile_set: \"/main/Cell/Cell.atlas\"\n"
-  "default_animation: \"Nucleolis_(biology)\"\n"
+  "default_animation: \"Cell_Nucleolis\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
@@ -56,8 +56,8 @@ embedded_components {
 embedded_components {
   id: "sprite_anim"
   type: "sprite"
-  data: "tile_set: \"/builtins/graphics/particle_blob.tilesource\"\n"
-  "default_animation: \"anim\"\n"
+  data: "tile_set: \"/main/Cell/Cell.atlas\"\n"
+  "default_animation: \"particle_blob\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
@@ -76,5 +76,51 @@ embedded_components {
     x: 40.0
     y: 40.0
     z: 1.0
+  }
+}
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "collision_shape: \"\"\n"
+  "type: COLLISION_OBJECT_TYPE_DYNAMIC\n"
+  "mass: 1.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"default\"\n"
+  "mask: \"default\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_SPHERE\n"
+  "    position {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 1\n"
+  "  }\n"
+  "  data: 226.0\n"
+  "}\n"
+  "linear_damping: 0.9\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  "bullet: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
   }
 }
---
+++ main/Аquarium/Аquarium.go
@@ -0,0 +1,47 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/main/\\320\\220quarium/\\320\\220quarium.atlas\"\n"
+  "default_animation: \"\\320\\220quarium_background\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "collision_shape: \"/main/\\320\\220quarium/\\320\\220quarium_Ground.tilemap\"\n"
+  "type: COLLISION_OBJECT_TYPE_DYNAMIC\n"
+  "mass: 1.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"default\"\n"
+  "mask: \"default\"\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  "bullet: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
