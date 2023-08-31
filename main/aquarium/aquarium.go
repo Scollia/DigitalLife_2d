90d7bb4cf29e1b4a4a5fd362155d28ef5320718f
Scollia <scollia@mail.ru>
2023-08-31 17:57:52 +0300

    Создание камеры

--- main/aquarium/aquarium.go
+++ main/aquarium/aquarium.go
@@ -17,8 +17,8 @@ components {
   id: "\320\220quarium_Ground"
   component: "/main/aquarium/aquarium_Ground.tilemap"
   position {
-    x: -648.0
-    y: -666.0
+    x: -896.0
+    y: -658.0
     z: 0.0
   }
   rotation {
@@ -29,7 +29,7 @@ components {
   }
 }
 embedded_components {
-  id: "sprite"
+  id: "sprite_aquarium_background"
   type: "sprite"
   data: "tile_set: \"/main/aquarium/aquarium.atlas\"\n"
   "default_animation: \"\\320\\220quarium_background\"\n"
@@ -37,7 +37,7 @@ embedded_components {
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
   position {
-    x: 91.0
+    x: -161.0
     y: 1.0
     z: -1.0
   }
@@ -49,7 +49,7 @@ embedded_components {
   }
 }
 embedded_components {
-  id: "sprite1"
+  id: "sprite_aquarium_frame"
   type: "sprite"
   data: "tile_set: \"/main/aquarium/aquarium.atlas\"\n"
   "default_animation: \"\\320\\220quarium_frame\"\n"
@@ -57,7 +57,7 @@ embedded_components {
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
   position {
-    x: 252.0
+    x: 0.0
     y: 0.0
     z: 1.0
   }
@@ -69,21 +69,21 @@ embedded_components {
   }
 }
 embedded_components {
-  id: "collisionobject"
+  id: "co_ground"
   type: "collisionobject"
   data: "collision_shape: \"/main/aquarium/aquarium_Ground.tilemap\"\n"
   "type: COLLISION_OBJECT_TYPE_STATIC\n"
   "mass: 0.0\n"
   "friction: 0.3\n"
   "restitution: 0.8\n"
-  "group: \"default\"\n"
+  "group: \"ground\"\n"
   "mask: \"default\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
   "    shape_type: TYPE_BOX\n"
   "    position {\n"
-  "      x: 834.0\n"
-  "      y: 1.0\n"
+  "      x: 582.0\n"
+  "      y: 0.0\n"
   "      z: 0.0\n"
   "    }\n"
   "    rotation {\n"
@@ -98,7 +98,7 @@ embedded_components {
   "  shapes {\n"
   "    shape_type: TYPE_BOX\n"
   "    position {\n"
-  "      x: 88.0\n"
+  "      x: -163.0\n"
   "      y: 671.0\n"
   "      z: 0.0\n"
   "    }\n"
@@ -114,8 +114,8 @@ embedded_components {
   "  shapes {\n"
   "    shape_type: TYPE_BOX\n"
   "    position {\n"
-  "      x: -651.0\n"
-  "      y: 2.0\n"
+  "      x: -903.0\n"
+  "      y: 0.0\n"
   "      z: 0.0\n"
   "    }\n"
   "    rotation {\n"
@@ -127,20 +127,87 @@ embedded_components {
   "    index: 6\n"
   "    count: 3\n"
   "  }\n"
-  "  data: 8.2415\n"
-  "  data: 664.3075\n"
-  "  data: 6.8\n"
-  "  data: 756.988\n"
+  "  data: 10.0\n"
+  "  data: 670.0\n"
+  "  data: 10.0\n"
+  "  data: 749.0\n"
   "  data: 10.0\n"
   "  data: 10.0\n"
   "  data: 10.0\n"
-  "  data: 670.0945\n"
+  "  data: 670.0\n"
   "  data: 10.0\n"
   "}\n"
   "linear_damping: 0.0\n"
   "angular_damping: 0.0\n"
   "locked_rotation: false\n"
   "bullet: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "co_water"
+  type: "collisionobject"
+  data: "collision_shape: \"\"\n"
+  "type: COLLISION_OBJECT_TYPE_TRIGGER\n"
+  "mass: 0.0\n"
+  "friction: 0.5\n"
+  "restitution: 0.5\n"
+  "group: \"water\"\n"
+  "mask: \"default\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: -163.0\n"
+  "      y: -108.0\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "  }\n"
+  "  data: 749.0\n"
+  "  data: 567.5\n"
+  "  data: 10.0\n"
+  "}\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  "bullet: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "factory_call"
+  type: "factory"
+  data: "prototype: \"/main/cell/cell.go\"\n"
+  "load_dynamically: false\n"
+  "dynamic_prototype: false\n"
   ""
   position {
     x: 0.0
